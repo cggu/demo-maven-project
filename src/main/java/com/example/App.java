70dc555eae8bad9521c922fcdc7ed06e729ff4de
cggu <cggu0605@gmail.com>
2025-05-04 14:51:14 +0800

    Update hello message to v2.0

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,23 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello, this is a demo Maven project for Jenkins CI/CD!");
+        SpringApplication.run(App.class, args);
+    }
+
+    @RestController
+    public static class HelloController {
+
+        @GetMapping("/hello")
+        public String hello() {
+            return "Hello, this is a demo Maven project for Jenkins CI/CD!";
+        }
     }
 }
